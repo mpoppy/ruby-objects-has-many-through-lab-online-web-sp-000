983578c80311fe59f66c42a48238649960f113ce
Marc Poppy <poppy.marc@gmail.com>
2019-07-01 05:45:27 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,6 +14,7 @@ class Patient
   def new_appointment(doctor, date)
     appointment = Appointment.new(date, self, doctor)
     @appointments << appointment
+    appointment
   end
 
   def appointments
