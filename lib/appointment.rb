4d897c5083a2bb1cfab01c727bdd71863ea19387
Marc Poppy <poppy.marc@gmail.com>
2019-07-01 06:12:10 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -13,7 +13,7 @@ class Appointment
   end
 
   def patient
-    Patient.appointments.select{|appointment| appointment.patient}
+    Patient.new_appointment.select{|appointment| appointment.patient}
   end
 
   def self.all
