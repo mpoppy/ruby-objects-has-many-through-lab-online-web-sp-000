7d6b0c415df89c57204f8fd4b1f2c98bd74b86b3
Marc Poppy <poppy.marc@gmail.com>
2019-07-01 05:33:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
 
   def patients
-    Appointment.all.select { |appoinment| appoinment.doctor = self  }
+    Appointment.all.map { |appoinment| appoinment.doctor = self  }
   end
 
   def self.all
